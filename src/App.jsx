a4c0c26a496ab4fab4115df5c74ad46bfc1d6c3f
cristian rios <cwrios@hotmail.com>
2022-08-22 22:19:58 -0300

    correccion segundo desafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
 import './App.css';
 import Form from './components/Form'
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <>
-    
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <div className="collapse navbar-collapse" id="navbarColor01">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home
-            <span className="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        
-      </ul>
-      <Form contBuscar="Productos"/>
-      </div>
-    
-  </div>
-</nav>
-    </>
+    <NavBar />
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+            {/* <Form contBuscar="Productos"/> */}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
